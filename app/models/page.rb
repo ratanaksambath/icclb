34dffffdaab8d41aa682b6fbaa4c6a3b902c5801
Andrew Ng <andrewkawing@gmail.com>
2015-05-28 16:22:22 -0700

    rm

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,6 @@
 class Page < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  before_action :authenticate_user!
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
