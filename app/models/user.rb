f75cb7e23f9d601ba291f7bfc64d10ef9615a17f
Andrew Ng <andrewkawing@gmail.com>
2015-06-22 18:24:12 -0700

    fix password changing

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ActiveRecord::Base
 	#inclusion: { in: proc { InviteCode.where( used: false ).map( &:code ) } }
 	inclusion: { in: ["springhearings1","springhearings2"] }
 
+	after_create :change_password
+
+	def change_password
+		  self.password = self.membership_type
+  		  self.password_confirmation = self.membership_type
+  		  self.save
+	end
+
 end
