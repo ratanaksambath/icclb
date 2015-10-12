d96aa9e632da101d5c687df806f54fd4e6a1af1e
ratanaksambath <ratanaksambath@gmail.com>
2015-10-12 11:28:43 -0700

    allow iframe access to the prodution

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module Videos
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+
+    # allow iframe access
+    config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
